fc3c4165a1b68f186baf3ffc735276b63b7df8a5
saikrishnanooka-vl <krishna.n@valuelabs.com>
2020-09-15 17:58:43 +0530

    Hiding review and updated home page logic

--- summit_blood_samples/blood_sample/views.py
+++ summit_blood_samples/blood_sample/views.py
@@ -37,135 +37,150 @@ class HomeTabView(LoginRequiredMixin, View):
         """
 
         # Getting Unmatched records comparing Blood sample with Manifest records
+        # query = '''
+        #     SELECT DISTINCT DATE(bs."CreatedAt"),\
+        #         to_char(bs."CreatedAt",'Month DD, YYYY' ) as dates
+        #     FROM blood_sample_bloodsample as bs
+        #     left join blood_sample_manifestrecords as mr on \
+        #         bs."Barcode" = mr."Barcode"
+        #     WHERE mr."id" is null and bs."State"='0'
+        #     ORDER BY DATE(bs."CreatedAt")
+        # '''
+        # with connection.cursor() as cursor:
+        #     cursor.execute(query)
+        #     bloodsample_dates = [
+        #         re.sub(' +', ' ', row[1])
+        #         for row in cursor.fetchall()
+        #     ]
+
+        # # Getting Unmatched records comparing Manifest with Blood Sample records
+        # query = '''
+        #     SELECT DISTINCT Date(mr."CollectionDateTime"), \
+        #         to_char(mr."CollectionDateTime",'Month DD, YYYY' ) as dates
+        #     FROM "blood_sample_manifestrecords" as mr
+        #     left join blood_sample_bloodsample as bs on \
+        #         bs."Barcode" = mr."Barcode"
+        #     WHERE bs."id" is null
+        #     ORDER BY Date(mr."CollectionDateTime") ASC
+        # '''
+        # with connection.cursor() as cursor:
+        #     cursor.execute(query)
+        #     un_manifest_dates = [
+        #         re.sub(' +', ' ', row[1])
+        #         for row in cursor.fetchall()
+        #     ]
+
+        # # Getting Unmatched records comparing Manifest with Receipt records
+        # query = '''
+        #     SELECT DISTINCT Date(mr."CollectionDateTime"), \
+        #         to_char(mr."CollectionDateTime",'Month DD, YYYY' ) as dates
+        #     FROM "blood_sample_manifestrecords" as mr
+        #     left join blood_sample_receiptrecords as rr on \
+        #         rr."Barcode" = mr."Barcode"
+        #     WHERE rr."id" is null
+        #     ORDER BY Date(mr."CollectionDateTime") ASC
+        # '''
+        # manifest_dates = []
+
+        # with connection.cursor() as cursor:
+        #     cursor.execute(query)
+        #     manifest_dates = [
+        #         re.sub(' +', ' ', row[1])
+        #         for row in cursor.fetchall()
+        #     ]
+
+        # # Getting Unmatched records comparing Receipt with Manifest records
+        # query = '''
+        #     SELECT DISTINCT Date(rr."DateTimeTaken"), \
+        #         to_char(rr."DateTimeTaken",'Month DD, YYYY' ) as dates
+        #     FROM "blood_sample_receiptrecords" as rr
+        #     left join blood_sample_manifestrecords as mr on \
+        #         rr."Barcode" = mr."Barcode"
+        #     WHERE mr."id" is null
+        #     ORDER BY Date(rr."DateTimeTaken") ASC
+        # '''
+        # un_receipt_dates = []
+        # with connection.cursor() as cursor:
+        #     cursor.execute(query)
+        #     un_receipt_dates = [
+        #         re.sub(' +', ' ', row[1])
+        #         for row in cursor.fetchall()
+        #     ]
+
+        # # Getting Unmatched records comparing Receipt with Processed records
+        # query = '''
+        #     SELECT
+        #         DISTINCT to_char(rr."DateTimeTaken",'Month DD, YYYY' )
+        #     FROM blood_sample_receiptrecords as rr
+        #     inner join blood_sample_manifestrecords as mr on \
+        #         (rr."Barcode" = mr."Barcode")
+        #     inner join blood_sample_bloodsample as bs on \
+        #         (bs."Barcode" = mr."Barcode")
+        #     WHERE rr."SampleId" not in (
+        #         SELECT
+        #             "pr"."ParentId"
+        #         FROM blood_sample_processedreport as pr
+        #         join blood_sample_receiptrecords as rr on \
+        #             pr."ParentId" = rr."SampleId"
+        #         join blood_sample_manifestrecords as mr on \
+        #             rr."Barcode" = mr."Barcode"
+        #         join blood_sample_bloodsample as bs on \
+        #             bs."Barcode" = mr."Barcode"
+        #     );
+        #     '''
+
+        # with connection.cursor() as cursor:
+        #     cursor.execute(query)
+        #     data_pr_rr_unmatched = [row[0]
+        #                             for row in cursor.fetchall()
+        #                             ]
+
+        # # Getting Unmatched records comparing Processed with Receipt records
+        # query = '''
+        #     SELECT DISTINCT to_char(pr."ProcessedDateTime",'Month DD, YYYY' )
+        #     FROM blood_sample_processedreport as pr
+        #     WHERE pr."ParentId" not in (
+        #             SELECT
+        #                 rr."SampleId"
+        #             FROM blood_sample_receiptrecords as rr
+        #             inner join blood_sample_manifestrecords as mr on \
+        #                 rr."Barcode" = mr."Barcode"
+        #             inner join blood_sample_bloodsample as bs on \
+        #                 bs."Barcode" = mr."Barcode"
+        #         ) ;
+        #     '''
+
+        # with connection.cursor() as cursor:
+        #     cursor.execute(query)
+        #     data_pr_unmatched = [
+        #         row[0]
+        #         for row in cursor.fetchall()
+        #     ]
+        # data_pr_rr_unmatched += data_pr_unmatched
+        # unmatched_pr_data = []
+        # [unmatched_pr_data.append(
+        #     x) for x in data_pr_rr_unmatched if x not in unmatched_pr_data]
+
+        # Getting the blood samples record dates where state is ACTIVE
         query = '''
-            SELECT DISTINCT DATE(bs."CreatedAt"),\
+            SELECT DISTINCT DATE(bs."CreatedAt"),
                 to_char(bs."CreatedAt",'Month DD, YYYY' ) as dates
             FROM blood_sample_bloodsample as bs
-            left join blood_sample_manifestrecords as mr on \
-                bs."Barcode" = mr."Barcode"
-            WHERE mr."id" is null and bs."State"='0'
+            WHERE bs."State"='0'
             ORDER BY DATE(bs."CreatedAt")
         '''
         with connection.cursor() as cursor:
             cursor.execute(query)
-            bloodsample_dates = [
+            bloodsample_active_dates = [
                 re.sub(' +', ' ', row[1])
                 for row in cursor.fetchall()
             ]
-
-        # Getting Unmatched records comparing Manifest with Blood Sample records
-        query = '''
-            SELECT DISTINCT Date(mr."CollectionDateTime"), \
-                to_char(mr."CollectionDateTime",'Month DD, YYYY' ) as dates
-            FROM "blood_sample_manifestrecords" as mr
-            left join blood_sample_bloodsample as bs on \
-                bs."Barcode" = mr."Barcode"
-            WHERE bs."id" is null
-            ORDER BY Date(mr."CollectionDateTime") ASC
-        '''
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            un_manifest_dates = [
-                re.sub(' +', ' ', row[1])
-                for row in cursor.fetchall()
-            ]
-
-        # Getting Unmatched records comparing Manifest with Receipt records
-        query = '''
-            SELECT DISTINCT Date(mr."CollectionDateTime"), \
-                to_char(mr."CollectionDateTime",'Month DD, YYYY' ) as dates
-            FROM "blood_sample_manifestrecords" as mr
-            left join blood_sample_receiptrecords as rr on \
-                rr."Barcode" = mr."Barcode"
-            WHERE rr."id" is null
-            ORDER BY Date(mr."CollectionDateTime") ASC
-        '''
-        manifest_dates = []
-
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            manifest_dates = [
-                re.sub(' +', ' ', row[1])
-                for row in cursor.fetchall()
-            ]
-
-        # Getting Unmatched records comparing Receipt with Manifest records
-        query = '''
-            SELECT DISTINCT Date(rr."DateTimeTaken"), \
-                to_char(rr."DateTimeTaken",'Month DD, YYYY' ) as dates
-            FROM "blood_sample_receiptrecords" as rr
-            left join blood_sample_manifestrecords as mr on \
-                rr."Barcode" = mr."Barcode"
-            WHERE mr."id" is null
-            ORDER BY Date(rr."DateTimeTaken") ASC
-        '''
-        un_receipt_dates = []
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            un_receipt_dates = [
-                re.sub(' +', ' ', row[1])
-                for row in cursor.fetchall()
-            ]
-
-        # Getting Unmatched records comparing Receipt with Processed records
-        query = '''
-            SELECT
-                DISTINCT to_char(rr."DateTimeTaken",'Month DD, YYYY' )
-            FROM blood_sample_receiptrecords as rr
-            inner join blood_sample_manifestrecords as mr on \
-                (rr."Barcode" = mr."Barcode")
-            inner join blood_sample_bloodsample as bs on \
-                (bs."Barcode" = mr."Barcode")
-            WHERE rr."SampleId" not in (
-                SELECT
-                    "pr"."ParentId"
-                FROM blood_sample_processedreport as pr
-                join blood_sample_receiptrecords as rr on \
-                    pr."ParentId" = rr."SampleId"
-                join blood_sample_manifestrecords as mr on \
-                    rr."Barcode" = mr."Barcode"
-                join blood_sample_bloodsample as bs on \
-                    bs."Barcode" = mr."Barcode"
-            );
-            '''
-
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            data_pr_rr_unmatched = [row[0]
-                                    for row in cursor.fetchall()
-                                    ]
-
-        # Getting Unmatched records comparing Processed with Receipt records
-        query = '''
-            SELECT DISTINCT to_char(pr."ProcessedDateTime",'Month DD, YYYY' )
-            FROM blood_sample_processedreport as pr
-            WHERE pr."ParentId" not in (
-                    SELECT
-                        rr."SampleId"
-                    FROM blood_sample_receiptrecords as rr
-                    inner join blood_sample_manifestrecords as mr on \
-                        rr."Barcode" = mr."Barcode"
-                    inner join blood_sample_bloodsample as bs on \
-                        bs."Barcode" = mr."Barcode"
-                ) ;
-            '''
-
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            data_pr_unmatched = [
-                row[0]
-                for row in cursor.fetchall()
-            ]
-        data_pr_rr_unmatched += data_pr_unmatched
-        unmatched_pr_data = []
-        [unmatched_pr_data.append(
-            x) for x in data_pr_rr_unmatched if x not in unmatched_pr_data]
-
         return render(request, self.template_name, {
-            'un_manifest_dates': un_manifest_dates,
-            'un_receipt_dates': un_receipt_dates,
-            'un_processed_dates': unmatched_pr_data,
-            'unmatched_manifest': bloodsample_dates,
-            'unmatch_receipt': manifest_dates,
+            # 'un_manifest_dates': un_manifest_dates,
+            # 'un_receipt_dates': un_receipt_dates,
+            # 'un_processed_dates': unmatched_pr_data,
+            # 'unmatched_manifest': bloodsample_dates,
+            # 'unmatch_receipt': manifest_dates,
+            'bloodsample_active_dates':bloodsample_active_dates,
         })
 
